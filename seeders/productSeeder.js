1416e3ba4811414bf7fc7bcb08eda580041fe102
Peter Slowak <p_slowak89@hotmail.com>
2024-03-28 13:08:33 -0300

    Update productSeeder.js

--- seeders/productSeeder.js
+++ seeders/productSeeder.js
@@ -26,7 +26,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Indulge in a nutty symphony with our Pistachio blend. Creamy, nutty undertones intertwine with rich coffee, creating a delightful fusion of flavors in every sip.",
       createdAt: "",
       updatedAt: "",
       color: "#74243d",
@@ -38,7 +38,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Escape to the tropics with our Coconut Cream blend. Luxuriously creamy coconut flavors meld with smooth coffee, offering a taste of paradise in every sip.",
       createdAt: "",
       updatedAt: "",
       color: "#74243d",
@@ -50,7 +50,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Dive into decadence with our Chocolate Amaretto blend. Rich chocolate and almond-infused notes dance harmoniously with bold coffee, creating an indulgent experience reminiscent of Italian elegance.",
       createdAt: "",
       updatedAt: "",
       color: "#700804",
@@ -62,7 +62,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Experience the celestial balance of our Half Moon blend. Robust dark roast meets smooth light roast, creating a captivating harmony perfect for any time of day.",
       createdAt: "",
       updatedAt: "",
       color: "#2e2a27",
@@ -74,7 +74,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Indulge in the warm, comforting embrace of our Cinnamon Bun blend. Rich cinnamon swirls delicately with creamy coffee, evoking the nostalgic delight of freshly baked buns.",
       createdAt: "",
       updatedAt: "",
       color: "#3ec2cf",
@@ -86,7 +86,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Venture into the depths of flavor with our Dark Adirondack blend. Robust and invigorating, it captures the rugged essence of the Adirondack wilderness in every bold sip.",
       createdAt: "",
       updatedAt: "",
       color: "#0c2639",
@@ -98,7 +98,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Immerse yourself in the bold intensity of Italy with our signature blend. Robust and full-bodied, it embodies the essence of Italian coffee culture in every rich and satisfying sip.",
       createdAt: "",
       updatedAt: "",
       color: "#0c2639",
@@ -110,7 +110,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Our hallmark brew, meticulously curated for every coffee enthusiast. With its harmonious flavor profile and comforting aroma, it's the perfect companion for every occasion, embodying the essence of home in every sip.",
       createdAt: "",
       updatedAt: "",
       color: "#0c2639",
@@ -122,7 +122,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "A bold and mysterious brew, offering a rich, velvety experience. Dark chocolate undertones entwine with smoky caramel hints, inviting you into the enigmatic depths of the night",
       createdAt: "",
       updatedAt: "",
       color: "#2e2a27",
@@ -134,7 +134,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Experience the perfect balance with our Half Caff blend. Delightfully smooth, it offers a milder caffeine experience without compromising on flavor, ensuring a satisfying cup every time.",
       createdAt: "",
       updatedAt: "",
       color: "#0c2639",
@@ -146,7 +146,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "From Colombia's fertile lands, our Organic blend offers a pure taste of nature's bounty. Smooth and vibrant with notes of cocoa and citrus, it's a testament to sustainability and quality.",
       createdAt: "",
       updatedAt: "",
       color: "#700804",
@@ -158,7 +158,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Discover the essence of Mexico's highlands in every sip. Sustainably grown and delicately crafted, this blend offers a nuanced flavor profile with hints of chocolate, toasted nuts, and a gentle sweetness.",
       createdAt: "",
       updatedAt: "",
       color: "#700804",
@@ -170,7 +170,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Embark on a journey to the lush landscapes of Sumatra with our Mandheling blend. Rich and full-bodied, it boasts earthy notes with hints of cocoa and spice, capturing the essence of Indonesia's finest coffee.",
       createdAt: "",
       updatedAt: "",
       color: "#700804",
@@ -182,7 +182,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Experience the richness of Guatemala's coffee heritage with our Santo Domingo blend. Bold and complex, it offers a symphony of flavors with hints of dark chocolate, caramel, and a touch of fruity sweetness.",
       createdAt: "",
       updatedAt: "",
       color: "#700804",
@@ -194,7 +194,7 @@ module.exports = async () => {
       stock: 5,
       image: "",
       description:
-        "This bold, smooth blend contains 40% dark-roasted Kona coffee. We blend it with select Latin American beans for a rich, lively cup of coffee.",
+        "Delve into the exotic flavors of Ethiopia with our Yirgacheffe blend. Renowned for its floral aroma and bright acidity, it offers a unique sensory journey with delicate notes of jasmine, citrus, and honey.",
       createdAt: "",
       updatedAt: "",
       color: "#700804",
