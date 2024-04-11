6ce9156e125e896f05fc89f4ec86c9288407b450
DViergutz <d.viergutz@hotmail.de>
2024-04-11 16:09:49 -0300

    edit product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,9 +5,7 @@ const router = express.Router();
 router.get("/", productController.index);
 router.post("/", productController.store);
 router.get("/:id", productController.show);
-
 router.patch("/:id", productController.update);
-
 router.delete("/:id", productController.destroy);
 
 module.exports = router;
